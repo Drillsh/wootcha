b2514e3015bf8528d40570f5817bc263963348aa
daniel <1371590@naver.com>
2020-10-05 19:33:53 +0900

    main 레이아웃작업중

---
+++ main/footer.php
@@ -0,0 +1,12 @@
+<!-- <div id="footer_content">
+            <p id="footer_logo">PHP 활용 프로젝트 | <span>박다니엘</span></p>
+            <ul id="download">
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+            <ul id="author">
+                <li>저자 문의 메일</li>
+                <li>- 메일 주소 : hidaniel9911@gamil.com</li>
+            </ul>
+        </div> -->
