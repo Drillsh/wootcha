084189931fd3286c40cf3014753bf3fc0fe37b05
daniel <1371590@naver.com>
2020-10-09 21:51:46 +0900

    main화면 작업

--- main/main.php
+++ main/main.php
@@ -1,3 +1,4 @@
+<!-- 상단 슬라이드 -->
 <div class="slideShow">
         <div class="slideShow_slides">
             <a href="#"><img src="http://<?=$_SERVER['HTTP_HOST']?>/wootcha/main/image/slide-1.jpg" alt="slide1"></a>
@@ -19,13 +20,74 @@
 
 <!-- 총 리스트 -->
 <div class="css-89vjyi-MainSection ebeya3l11">
-  
+<!-- 타이틀과 영화항목 리스트 -->
   <div class="css-gxko42-StyledHomeListContainer ebeya3l2">
+    <!-- 타이틀 -->
+    <div class="css-1ewd6nb-StyledHomeListTitleRow-StyledHomeListTitleRow ebeya3l3">
+      <!-- 타이틀 설정 -->
+      <p class="css-1e8eq80-StyledHomeListTitle ebeya3l6">베스트 리뷰</p>
+    </div>
+    <!-- 영화항목 -->
+    <div class="css-gc1vu8-StyledHorizontalScrollOuterContainer ebeya3l4">
+      <!-- 영화 로비 -->
+      <div class="css-cjxm4v-ScrollBarContainer e1f5xhlb0">
+        <!-- 영화 1층 -->
+        <div class="css-chidac-ScrollBar e1f5xhlb1">
+          <!-- 영화 2층 -->
+          <div class="css-150y45-ScrollingInner e1f5xhlb2">
+            <!-- 영화 3층 -->
+            <div class="css-6kwoq4-StyledHorizontalScrollInnerContainer ebeya3l5">
+              <!-- 영화 4층 ul -->
+              <ul class="ebeya3l0 css-wvdjot-VisualUl-StyledHorizontalUl-StyledHorizontalUlWithContentPosterList-RowList eykn4p10">
+                <!-- 영화 5층 li -->
+                <li class="css-106b4k6-Self e3fgkal0">
+                  <!-- 영화 6층 a -->
+                  <a title="" href="#">
+                    <!-- 6층 포스터 -->
+                    <div class="css-wg9zzb-ContentPosterBlock e3fgkal1">
+                      <!-- 포스터 설정 -->
+                      <div class=" e1pon7hn0 css-1hdc5d8-Self-LazyLoadingImg ewlo9840">
+                        <img src="" class=" e1pon7hn0 css-1onlrbk-Img-LazyLoadingImg ewlo9841">
+                      </div>
+                      <!-- 포스터 랭크 -->
+                      <div class="css-13ot87v-RankBadge e3fgkal7">1</div>
+                    </div>
+                    <!-- 6층 설명 -->
+                    <div class="css-dmreg0-ContentInfo e3fgkal2">
+                      <!-- 영화설명_제목 -->
+                      <div class="css-1teivyt-ContentTitle e3fgkal3">영화제목</div>
+                      <!-- 영화설명_출시년도 -->
+                      <div class="css-6t186m-StyledContentYearAndNation ebeya3l12">2020</div>
+                      <!-- 영화설명_평점 -->
+                      <div class="average css-uaqea0-ContentRating-StyledContentRating ebeya3l14">
+                        <span>평점</span>
+                          <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 12 12" fill="#555765" class="css-a4gmui-IcRatingStarSvg erjycaa0">
+                            <path class="fillTarget" fill="#6A6B76" fill-rule="evenodd" d="M5.637 8.02L2.779 9.911c-.138.092-.324.054-.415-.084-.048-.073-.063-.162-.04-.246l.916-3.302L.56 4.145c-.13-.103-.152-.292-.048-.421.054-.068.134-.11.221-.113l3.424-.15 1.2-3.21c.058-.155.23-.233.386-.175.081.03.146.094.176.175l1.2 3.21 3.424.15c.165.007.294.147.286.313-.003.086-.045.167-.112.221L8.034 6.28l.915 3.302c.045.16-.049.325-.209.37-.083.022-.173.008-.245-.04L5.637 8.02z"></path>
+                          </svg>
+                        <span>실제점수</span>
+                      </div>
+                    </div>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <!-- 영화로비_양쪽 틈 -->
+        <div direction="left" class="css-7lqh27-CheatBlock e1f5xhlb4"></div>
+        <div direction="right" class="css-xypm4l-CheatBlock e1f5xhlb4"></div>
+        <!-- 영화로비_오른쪽버튼 -->
+        <div class="arrow_button css-19lj8ig-ArrowButtonBlock e1f5xhlb3" direction="left">
+          <div class="css-1o4i6uc-BackwardButton e1f5xhlb6"></div>
+        </div>
+        <!-- 영화로비_왼쪽버튼 -->
 
+      </div> <!-- END 영화로비 -->
+    </div>
   </div>
 </div>
 
-
+<!-- 기존 영화리스트 -->
 <div class="main_center">
 <div class="main_all">
 
@@ -34,6 +96,7 @@
     <?php
       include_once $_SERVER['DOCUMENT_ROOT']."/wootcha/common/database/db_connector.php";
       include_once $_SERVER['DOCUMENT_ROOT']."/wootcha/common/crawling/movie_cgv_crawling.php";
+      include_once $_SERVER['DOCUMENT_ROOT']."/wootcha/search/movie_naver_api_func.php";
 
       $sql = "select * from review R inner join movie M on R.mv_num = M.mv_num order by review_like desc limit 5";
       $result = mysqli_query($con, $sql);
@@ -88,6 +151,7 @@
         $review_long = $row['review_long'];
         $mv_title = $row['mv_title'];
         $img_link = get_cgv_movie_middle_poster_url($mv_title);
+
       ?>
 
             <a class="main_a_contentbox" href="#">
